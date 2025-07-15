372ea628585c66e37bfddd8a938f99c2b1ad0785
dinesh <din637451@gmail.com>
2025-07-15 20:40:32 +0530

    BasicJs

---
+++ Divisibleof5and3.js
@@ -0,0 +1,8 @@
+const a=15;
+if(a%3==0&&a%5==0)
+{
+    console.log("The no.is divisible by 3 and 5");
+}
+else{
+    console.log("The no.is not divisible by 3 and 5")
+}
---
+++ Greatestoftwo.js
@@ -0,0 +1,8 @@
+const a=10;
+const b=20;
+if(a>b){
+    console.log("A is Greater");
+}
+else{
+    console.log("B is greater");
+}
