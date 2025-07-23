4ecfad29eb5fb4a889d8457fda65bd8e091d55bd
dinesh <din637451@gmail.com>
2025-07-23 22:22:26 +0530

    Basicjs

---
+++ ForEachLoop.js
@@ -0,0 +1,25 @@
+let fruits={"apple","jack fruit","mango"};
+fruits.forEach(function(fruits.index)(
+    console.log(index="+")
+))
+let n=(1,2,3),
+let doubled=n.map((num)=>num*2);
+console.log(doubled);
+
+let num=[1,2,3];
+let doub=[];
+num.foreach((n1))
+
+
+let person={
+    name:"Ram",
+    age:47,
+    isStudent:true};
+console.log(person.city);
+delete person.isStudent;
+console.log(person.isStudent);
+
+for(let key in person)
+{
+    console.log(key+":"+person:[key]);
+}   
